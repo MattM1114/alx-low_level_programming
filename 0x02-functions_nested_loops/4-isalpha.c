e5cfff0afeac94497552af3010ff575defe2aeec
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-13 17:41:26 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  * _isaplha - main function
  * @c: character to compare
- * Return: 1 if ture, 0 if false
+ * Return: 1 if true, 0 if false
  */
 int _isalpha(int c)
 {
-	if ((C >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
 	{
 		return (1);
 	}
