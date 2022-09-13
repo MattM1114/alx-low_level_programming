8f314f86662db62552e6377e819ccd094dc75614
MattM1114 <matt.magadza@gmail.com>
2022-09-13 14:02:49 -0700

    11 fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,38 +7,22 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
-			printf("%d,", n);
-			if (n != 98)
-			{
-				printf(",");
-			}
-			n--;
+			printf("%d,",n--);
+			printf("%d,",n);
 		}
 	}
-	else if (n < 98)
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(",");
-			}
-			n++;
-		}
-	}
+
 	else
 	{
-		printf("98");
-		printf("81");
-		printf("111");
-		printf("0");
-		printf("-98");
-		printf("-111");
+		while (n < 98)
+		{
+			printf("%d,",n++);
+                       	printf("%d,",n);
+		}
 	}
-	printf("\n");
 }
+
