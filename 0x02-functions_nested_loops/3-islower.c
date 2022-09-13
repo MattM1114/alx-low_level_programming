9724e782b8bb1ceb589562dbfa519e6f4dc821f9
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-13 17:34:56 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,10 +4,10 @@
  * @c: character to compare
  * Return: 1 if true, 0 if false
  */
-int _islower(int c)
+int _islower (int c)
 {
-	if(c >= 'a' && c <= 'z')
-		return(1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
 	else
-		return(0);
+		return (0);
 }
