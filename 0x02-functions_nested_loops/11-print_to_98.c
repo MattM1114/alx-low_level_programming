dcd4cd567f9df2446eb8873f3494a29833e3a3c0
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-13 23:05:02 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,8 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d,",n--);
-			printf("%d,",n);
+			printf("%d,", n--);
+			printf("%d,", n);
 		}
 	}
 
@@ -20,8 +20,8 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d,",n++);
-                       	printf("%d,",n);
+			printf("%d,", n++);
+            printf("%d,", n);
 		}
 	}
 }
