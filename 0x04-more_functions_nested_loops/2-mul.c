0f80402f4b9f43766bcf3545e02b8cc037f01bdd
MattM1114 <matt.magadza@gmail.com>
2022-09-15 04:54:32 -0700

    2 fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * mul - checks for checks for a digit (0 though 9).
+ * mul - function that multiplies two integers.
  * @a ; a - variable
  * @b ; b - variable
  * Return: addition of a and b
