1fc5213a95d89a196d29b1933e04be9b79b406c1
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-15 15:19:30 +0200

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,14 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = n - i; j < n; j++)
 		}
 		_putchar('\\');
 		_putchar('\n');
