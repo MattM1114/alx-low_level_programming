76bf1b915c90d3bf886ab595db8399956a7acfab
MattM1114 <matt.magadza@gmail.com>
2022-09-15 05:39:26 -0700

    4 fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
  * print_most_numbers - prints 0 - 9 with a new line, exclude 2 and 4
- *
+ * Description: Uses headers to link and a nested loops to achieve goal
  * Return: void. no return.
  */
 void print_most_numbers(void)
 {
-	int a;
+	int a = 0;
 
 	for (a = 0; a< 10; a++)
-	{
 		if (a != 2 && a != 4)
-		{
 			_putchar(a + '0');
-		}
 		_putchar('\n');
 }
+
