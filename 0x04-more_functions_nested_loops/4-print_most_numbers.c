9614cb1edf542b967ae6e942af8e10dbeba11c63
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-15 14:50:42 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,13 +11,9 @@ void print_most_numbers(void)
 	for (a = 0; a < 10; a++)
 	{
 		if (a == 2 || a == 4)
-		{
 			continue;
-		}
 		else
-		{
 			_putchar(a + '0');
-		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
-
