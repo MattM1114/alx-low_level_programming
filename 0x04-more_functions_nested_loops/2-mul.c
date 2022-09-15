e6c8f9ca4b6dee38c63c9e5067a8294fcf303915
MattM1114 <matt.magadza@gmail.com>
2022-09-15 04:36:44 -0700

    2 fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
 /**
  * mul - checks for checks for a digit (0 though 9).
  * @a ; a - variable
  * @b ; b - variable
  * Return: addition of a and b
  */
-
 int mul(int a, int b)
-{ 
+{
 	return (a * b);
 }
 
