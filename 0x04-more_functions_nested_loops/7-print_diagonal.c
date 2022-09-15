04183c6351a8aaac1d40f6d21486053e89294a03
MattM1114 <matt.magadza@gmail.com>
2022-09-15 06:10:29 -0700

    7 made

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line with new line
+ * @n: an int given by main
+ *
+ * Description: uses headers to link and a nested loops to achieve goal
+ * Return: void. no return.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+
