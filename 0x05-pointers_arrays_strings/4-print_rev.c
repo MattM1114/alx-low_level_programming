cc68ef980c538ec0ecf56601c65796ae78f5015b
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-19 15:29:40 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,16 @@
 /**
  * print_rev - prints a string in stdout in reverse
  * @s: string to print 
- *
  * Return: void 
  */
 void print_rev(char *s)
 {
 	int i = 0;
 
-	while(*(s + i))
+	while (*(s + i))
 		i++;
 	i = i - 1;
-	while(i >= 0)
+	while (i >= 0)
 	{
 		_putchar(*(s + i));
 		i--;
