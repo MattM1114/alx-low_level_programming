cf94d344145d4094b62993f115389044ee184c9b
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-21 19:01:31 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,26 @@
 #include "main.h"
-
 /**
- * leet - encodes a string 1337.
- * @s: The string to be encode.
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
  * Return: A pointer to the encoded string.
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int a = 0, b;
-	char leet[8] = {'O', 'L', '?', 'E'}
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 <= 7; indx2++)
+		{
+			if (str[indx1] == leet[indx2] ||
+				str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
+		}
+
+		indx1++;
+	}
+
+	return (str);
+}
