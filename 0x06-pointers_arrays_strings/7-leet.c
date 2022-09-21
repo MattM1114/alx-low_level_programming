2b2728efa8dab518adf41e35a28970b42548960a
MattM1114 <matt.magadza@gmail.com>
2022-09-21 05:24:56 -0700

    1 - 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string 1337.
+ * @s: The string to be encode.
+ * Return: A pointer to the encded string.
+ */
+char *leet(char *s)
+{
+	int a = 0, b = 0, c = 5;
+	char d[5] = {'A', 'E', 'O', 'T', 'L'};
+	char e[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (b < 1)
+		{
+			if (s[a] == r[b] || s[a] - 32 == d[b])
+			{
+				s[a] = n[b];
+			}
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
+
