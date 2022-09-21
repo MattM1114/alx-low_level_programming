82489e746501eb7452a76a6db1f30bb299d347e6
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-09-21 19:05:56 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * rot13 - Encodes a string using rot13.
- * @s: The string to encode
+ * rot13 - ...
+ * @s: ...
  *
- * Return: A pointer to the encoded string.
+ * Return: ...
  */
 char *rot13(char *s)
 {
@@ -12,17 +13,20 @@ char *rot13(char *s)
 
 	while (s[a])
 	{
-		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'z'))
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
 		{
 			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
 			{
 				s[a] -= 13;
 				break;
 			}
+
 			s[a] += 13;
 			break;
 		}
+
 		a++;
 	}
+
 	return (s);
 }
