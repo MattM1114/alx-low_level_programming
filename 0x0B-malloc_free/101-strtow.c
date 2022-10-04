31390a46741bfe74732e4faf7b1130aa78284aa7
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-10-04 10:04:50 +0200

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,15 +14,18 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
 			len++;
 	}
 	len += ac;
+
 	str = malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
