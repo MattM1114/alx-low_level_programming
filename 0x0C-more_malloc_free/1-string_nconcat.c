6b2fc41152be1dae458a77dde9f1918513775abf
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-10-06 13:44:29 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *str;
-
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -22,17 +21,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-
 	while (s1[i])
 	{
 		i++;
 	}
-
 	while (s2[k])
 	{
 		k++;
 	}
-
 	if (n >= k)
 	{
 		l = i + k;
@@ -41,7 +37,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		l = i +n;
 	}
-
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 	{
