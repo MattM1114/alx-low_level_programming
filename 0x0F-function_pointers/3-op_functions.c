41395dfcb72b6162096025db1ab216a72d284940
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-10-12 10:17:33 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,7 +9,7 @@
 */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 /**
 * op_mul - 5 functions
@@ -19,7 +19,7 @@ int op_add(int a, int b)
 */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 /**
 * op_div - 5 functions
@@ -29,7 +29,7 @@ int op_mul(int a, int b)
 */
 int op_div(int a, int b)
 {
-    return (a / b);
+	return (a / b);
 }
 /**
 * op_mod - 5 functions
@@ -39,5 +39,5 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
