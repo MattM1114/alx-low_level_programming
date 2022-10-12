cffd37c9379e588ec851a4155f3f30a07fb9e225
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-10-12 09:37:20 +0200

    start of 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -21,4 +21,3 @@ typedef struct op
 } op_t;
 
 #endif
-
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,2 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
+/**
+* get_op_func -matches operator from main
+* @s: op str
+* Return: 0
+*/
