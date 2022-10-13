8f3d9e92d18f2e70a2e2a0186d8369f19ab9ce6e
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-10-13 13:29:43 +0200

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,30 @@
+#include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
- * sum_them_all - sum of variadic variable
- * @n:const
- * Return:int
+ * sum_them_all - func that returns the sum of all parameters.
+ * @n: type unsigned int
+ * Return: sum
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
-va_list list;
-double sum = 0;
+	int sum;
+	unsigned int x;
+	va_list valist;
+
 
 	if (n == 0)
-	{
 		return (0);
-	}
-	if (n == 0)
+
+	va_start(valist, n);
+
+	sum = x = 0;
+	while (x < n)
 	{
-		return (0);
+		sum = sum + va_arg(valist, int);
+		x++;
 	}
-va_start(list, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(list, int);
-	}
-	va_end(list);
+	va_end(valist);
 	return (sum);
 }
