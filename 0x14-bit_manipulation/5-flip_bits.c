7bb1b0e9204bae7b734826544dfd1211dad586cc
MattM1114 <matt.magadza@gmail.com>
2022-10-27 06:12:30 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,10 +13,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int bits;
 
 	for (bits = 0; n || m; n >>= 1, m >>= 1)
-		{
-			if ((n & 1) != (m & 1))
-				bits++;
-		}
+	{
+		if ((n & 1) != (m & 1))
+			bits++;
+	}
 	return (bits);
 }
 
